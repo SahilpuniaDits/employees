68021f281c0d7085ccb85f6fb23b5f7757bea348
arpan saini <arpansainiwins@gmail.com>
2021-12-27 16:11:44 +0530

    ...........

--- leaves/models.py
+++ leaves/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+# from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+
+  # Roles created here
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField('Email',unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    is_staff = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    # is_superuser=models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+  
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- leaves/urls.py
+++ leaves/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     
     path('login/', login , name = 'login'),
     path('signup/', signup, name= 'signup'),
+    path('api/register', UserRegistrationView.as_view(), name='register'),
+    path('api/login/', UserLoginView.as_view(), name='login'),
     
 ]
