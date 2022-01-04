6a0c608b7a05c5d6b541e32860e7b0aa54f0b1bb
arpan saini <arpansainiwins@gmail.com>
2022-01-04 09:44:43 +0530

    script

--- leaves/static/js/script.js
+++ leaves/static/js/script.js
@@ -1,55 +1,68 @@
-var email ;
+var email;
 var password;
 var email2;
 var password2;
 
-function login(){
+function login() {
+
     email = document.getElementById("Lemail").value
     password = document.getElementById("Lpassword").value
-    
+
     console.log(email)
     console.log(password)
 
-    fetch("http://127.0.0.1:8000/api/login/", {
+        fetch("http://127.0.0.1:8000/api/login/", {
 
-        method: "POST",
-        body: JSON.stringify({
-            email: email,
-            password: password,
-        }),
-        headers: {
-            "content-type": "application/json ; charset=UTF-8"
-        },
+            method: "POST",
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+            headers: {
+                "content-type": "application/json ; charset=UTF-8"
+            },
 
-    })
-        .then(function (data) {
-           
-            console.log(data);
-            return data.json();
         })
-        .then(function (data) {
-            alert(data)
-            console.log(data.success);
-          
-            a = data.success;
+            .then(function (data) {
 
-            if (a==true){
-                alert("1234")
-                location.href = "https://www.youtube.com/"
-            }
+                console.log(data);
+                if (data.status === 200) {
+                    location.href = "https://www.youtube.com/"
+                } else {
+                    alert("You entered wrong username or password")
+                }
 
-            else{
-                alert("You entered wrong username or password")
-            }
-
-
-           
+            })
             
-           
+        // (async () => {
+        //     const rawResponse = await fetch("http://127.0.0.1:8000/api/login/", {
+        //         method: 'POST',
+        //         headers: {
+        //             'Accept': 'application/json',
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify({
+        //             email: email,
+        //             password: password,
+        //         })
+        //     });
+        //     const content = await rawResponse.json();
 
-        })
+        //     console.log("--------->", content);
 
-    }
+        // }).then(function (data) {
+
+        //     console.log(data);
+        //     if (data.status === 200) {
+        //         location.href = "https://www.youtube.com/"
+        //     } else {
+        //         alert("You entered wrong username or password")
+        //     }
+
+        // });
+
+
+}
 
 
 
