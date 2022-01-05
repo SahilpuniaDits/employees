1f4edb6542375725475f0a6e614b00241301f49a
SahilpuniaDits <sahilpuniawins@gmail.com>
2022-01-05 14:59:38 +0530

    .

--- leaves/static/js/script.js
+++ leaves/static/js/script.js
@@ -1,22 +1,16 @@
 
 var email1;
 var password1;
-
 var email2;
 var password2;
 
 function login2() {
-
     email1 = document.getElementById("Lemail").value
     password1 = document.getElementById("Lpassword").value
-
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-
     console.log(email1)
     console.log(password1)
-
     alert(csrftoken)
-
     fetch("/api/login/", {
         method: "POST",
         body: JSON.stringify({
@@ -26,9 +20,7 @@ function login2() {
         headers: {
             "content-type": "application/json ; charset=UTF-8",
             'X-CSRFToken': csrftoken
-
         },
-
     })
         .then(function (data) {
             console.log(data);
@@ -37,14 +29,11 @@ function login2() {
 
             if (a == 200) {
                 window.location.href = "https://www.youtube.com/";
-
             }
-
             else {
                 alert("enter correct username or password...")
             }
         })
-
 }
 
 
@@ -53,24 +42,17 @@ function login2() {
 function signup() {
     email2 = document.getElementById("Semail").value
     password2 = document.getElementById("Spassword").value
-
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-
     console.log(email2)
     console.log(password2)
     alert(csrftoken)
-
     console.log("arpan")
-
-
     fetch("http://127.0.0.1:8000/api/register",
         {
-
             method: "POST",
             body: JSON.stringify({
                 email: email2,
                 password: password2,
-
             }),
             headers: {
 
@@ -89,30 +71,20 @@ function signup() {
 
             location.href = "http://127.0.0.1:8000/login/";
         })
-
-
-
-
 }
 var start_date;
 var end_date;
 var leave_Reason;
 var leave_commet;
 
-
-
 function leavesfatch() {
-    alert("aaaaaaaaa")
-
+    // alert("aaaaaaaaa")
     start_date = document.getElementById("sdate").value;
     end_date = document.getElementById("edate").value;
     leave_Reason = document.getElementById("reason").value;
     leave_commet = document.getElementById("comt").value;
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-
-
-
-    fetch("http://127.0.0.1:8000/api/leaves/", {
+    fetch("http://127.0.0.1:8000/api/applyleaves/", {
         method: "POST",
         body: JSON.stringify({
 
@@ -134,22 +106,17 @@ function leavesfatch() {
         });
 }
 
-fetchData()
 
 var html = "";
-
 function fetchData() {
+    var html = "";
     // alert("************************")
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-
-
     fetch(`http://127.0.0.1:8000/api/leavesget/`, {
         // method: "GET",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
             'X-CSRFToken': csrftoken
-
-
         },
     })
         .then((response) => response.json())
@@ -160,40 +127,30 @@ function fetchData() {
                 var id = data.id;
                 console.log(id);
                 html += `
-                        <tr>
-                            <td>${data.id}</td>
-                            <td>${data.startdate}</td>
-                            <td>${data.enddate}</td>
-                            <td>${data.reason}</td>
-                            <td>${data.comments}</td>
-                                       
-                                         
-                                        <td>
-                                            <a href="">
-                                                <button type="button"  class="btn btn-danger  appoin"  data-toggle="modal" data-target="#exampleModal2">
-                                                    <i class="fa fa-pencil text-warning" ></i>
-                                                </button>
-                                            </a>
-                                           
-                                            <a href="">
-                                                <button type="button" class="btn mx-1" onclick = "deleteCategory(${id})">
-                                                    <i class="fa fa-trash text-danger"></i>
-                                                </button>
-                                            </a>
-                                        </td>
-                          </tr> `;
-
-
+                    <tr>
+                        <td>${data.id}</td>
+                        <td>${data.startdate}</td>
+                        <td>${data.enddate}</td>
+                        <td>${data.reason}</td>
+                        <td>${data.comments}</td>                                  
+                        <td>
+                            <a href="">
+                                <button type="button"  class="btn btn-danger  appoin"  data-toggle="modal">
+                                    <i class="fa fa-pencil text-warning" ></i>
+                                </button>
+                            </a>                          
+                            <a href="">
+                                <button type="button" class="btn mx-1" onclick = "deleteCategory(${id})">
+                                    <i class="fa fa-trash text-danger"></i>
+                                </button>
+                            </a>
+                        </td>
+                    </tr> `;
             });
             document.getElementById("table1").innerHTML = html;
-
         })
-
-
 }
-
 fetchData();
-
 
 
 function deleteCategory(id) {
@@ -212,10 +169,10 @@ function deleteCategory(id) {
         .then((data) => {
             alert("Do you want to Delete this data?");
             showCategory();
-            console.log(data)
+            // console.log(data)
 
         });
 }
 
-// fetchData();
+
 
